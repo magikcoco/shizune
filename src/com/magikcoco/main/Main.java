1d550839d16c11065122d607a308e98d9fe1f164
homepc <punishedtomato@gmail.com>
2023-02-02 10:10:23 -0500

    Added functionality to main for handling arguments, added a textfile to keep track of what exit codes mean

--- src/com/magikcoco/main/Main.java
+++ src/com/magikcoco/main/Main.java
@@ -1,12 +1,35 @@
 package com.magikcoco.main;
 
 public class Main {
-    //TODO: query database on startup to load information
-    //TODO: handle arguments for database connection
-    //TODO: handle arguments for token file for discord bot
+
     //TODO: basic shell for exit, help, information commands
     //TODO: multithread functionality
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        if(args.length > 0){
+            int argCounter = 0;
+            String absolutePath = "";
+            String connectionString = "";
+            for(String arg : args){
+                if(arg.toLowerCase().startsWith("-t")){
+                    argCounter++;
+                    absolutePath = arg.substring(2);
+                } else if(arg.toLowerCase().startsWith("-d")){
+                    argCounter++;
+                    connectionString = arg.substring(2);
+                }
+            }
+            if(argCounter == 2){
+                //TODO: query database on startup to load information
+                //TODO: read tokenfile and pass it to a BotManager
+                System.out.println(absolutePath+connectionString);
+            } else {
+                System.out.println("Use: -t\"absolute/path/to/plaintext/tokenfile\" -d\"mongoDB-connection-string\"");
+                System.exit(9001);
+            }
+        } else {
+            System.out.println("Use: -t\"absolute/path/to/plaintext/tokenfile\" -d\"mongoDB-connection-string\"");
+            System.exit(9000);
+        }
     }
 }
