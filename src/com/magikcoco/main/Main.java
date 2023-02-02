c2739f8345b828095e0e8e55e061f064967b0faa
homepc <punishedtomato@gmail.com>
2023-02-02 09:47:27 -0500

    Create Main.java which prints Hello World to the console

---
+++ src/com/magikcoco/main/Main.java
@@ -0,0 +1,12 @@
+package com.magikcoco.main;
+
+public class Main {
+    //TODO: query database on startup to load information
+    //TODO: handle arguments for database connection
+    //TODO: handle arguments for token file for discord bot
+    //TODO: basic shell for exit, help, information commands
+    //TODO: multithread functionality
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
